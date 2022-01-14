7950d0aee4bc53e7e86047fc620ac08536910bea
Bggopal <get.sbggopal@gmail.com>
2022-01-14 08:10:32 +0530

    Intial commit

---
+++ ArduCAM_ESP32_DRIVE/ArduCAM_ESP32_DRIVE.ino
@@ -0,0 +1,423 @@
+// ArduCAM demo (C)2017 Lee
+// Web: http://www.ArduCAM.com
+// This program is a demo of how to use most of the functions
+// of the library with a supported camera modules, and can run on ArduCAM ESP32 UNO platform.
+// This demo was made for Omnivision  2MP/5MP sensor.
+// It will run the ArduCAM ESP32 2MP/5MP as a real 2MP/5MP digital camera, provide both JPEG capture.
+// The demo sketch will do the following tasks:
+// 1. Set the sensor to JPEG mode.
+// 2. Capture and buffer the image to FIFO every 5 seconds
+// 3. Store the image to Micro SD/TF card with JPEG format in sequential.
+// 4. Resolution can be changed by myCAM.set_JPEG_size() function.
+// This program requires the ArduCAM V4.0.0 (or later) library and ArduCAM ESP8266 2MP/5MP shield
+// and use Arduino IDE 1.8.1 compiler or above
+
+//https://script.google.com/macros/s/AKfycbyPsgB0ZXwPGi626o5L79yzt7XSy2uYj3n0CkZy8joJ46Z65no/exec
+
+#include <ArduCAM.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <Wire.h>
+#include <SPI.h>
+#include "FS.h"
+#include "SPIFFS.h"
+//#include "crypto/base64.h"
+#include "Base64.h" 
+#include "memorysaver.h"
+#if !(defined ESP32 )
+#error Please select the ArduCAM ESP32 UNO board in the Tools/Board
+#endif
+
+//This demo can work on OV2640_MINI_2MP/OV5640_MINI_5MP_PLUS/OV5642_MINI_5MP_PLUS/OV5642_MINI_5MP_PLUS/
+//OV5642_MINI_5MP_BIT_ROTATION_FIXED/ ARDUCAM_SHIELD_V2 platform.
+#if !(defined (OV2640_MINI_2MP)||defined (OV2640_MINI_2MP_PLUS)||defined (OV5640_MINI_5MP_PLUS) || defined (OV5642_MINI_5MP_PLUS) \
+    || defined (OV5642_MINI_5MP) || defined (OV5642_MINI_5MP_BIT_ROTATION_FIXED) \
+    ||(defined (ARDUCAM_SHIELD_V2) && (defined (OV2640_CAM) || defined (OV5640_CAM) || defined (OV5642_CAM))))
+#error Please select the hardware platform and camera module in the ../libraries/ArduCAM/memorysaver.h file
+#endif
+
+#define FORMAT_SPIFFS_IF_FAILED true
+
+const int CAM_POWER_ON = D10;
+// set GPIO16 as the slave select :
+const int CS = 5;
+//Version 2,set GPIO0 as the slave select :
+char pname[20];
+//static int  index=0;
+byte buf[256];
+static int i = 0;
+static int k = 0;
+uint8_t temp = 0, temp_last = 0;
+uint32_t length = 0;
+bool is_header = false;
+
+#if defined (OV2640_MINI_2MP)||defined (OV2640_MINI_2MP_PLUS) || defined (OV2640_CAM)
+ArduCAM myCAM(OV2640, CS);
+#elif defined (OV5640_MINI_5MP_PLUS) || defined (OV5640_CAM)
+ArduCAM myCAM(OV5640, CS);
+#elif defined (OV5642_MINI_5MP_PLUS) || defined (OV5642_MINI_5MP) || defined (OV5642_MINI_5MP_BIT_ROTATION_FIXED) ||(defined (OV5642_CAM))
+ArduCAM myCAM(OV5642, CS);
+#endif
+
+const char* ssid = "Nokia 6";
+const char* password = "bbbbbbbb";
+
+String myScript = "/macros/s/AKfycbyPsgB0ZXwPGi626o5L79yzt7XSy2uYj3n0CkZy8joJ46Z65no/exec";    //設定Google Script路徑
+//https://script.google.com/macros/s/AKfycbyPsgB0ZXwPGi626o5L79yzt7XSy2uYj3n0CkZy8joJ46Z65no/exec
+String myFoldername = "&myFoldername=ESP32-CAM";    //設定Google drive存放影像資料夾名
+String myFilename = "&myFilename=ESP32-CAM.jpg";    //設定Google drive存放影像檔名 (檔名格式：上傳時間+"_"+檔名)
+String myImage = "&myFile=";
+
+
+/*void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(500);
+  }
+  Serial.println("WiFi connected");
+  Serial.println("");
+  Serial.println(WiFi.localIP());
+}*/
+
+void capture2SD(fs::FS &fs, const char * path) {
+  File file ;
+  //Flush the FIFO
+  myCAM.flush_fifo();
+  //Clear the capture done flag
+  myCAM.clear_fifo_flag();
+  //Start capture
+  myCAM.start_capture();
+  Serial.println(F("Star Capture"));
+  while (!myCAM.get_bit(ARDUCHIP_TRIG , CAP_DONE_MASK));
+  Serial.println(F("Capture Done."));
+  length = myCAM.read_fifo_length();
+  Serial.print(F("The fifo length is :"));
+  Serial.println(length, DEC);
+  if (length >= MAX_FIFO_SIZE) //8M
+  {
+    Serial.println(F("Over size."));
+  }
+  if (length == 0 ) //0 kb
+  {
+    Serial.println(F("Size is 0."));
+  }
+  file = fs.open(path, FILE_WRITE);
+  if (!file) {
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+  i = 0;
+  myCAM.CS_LOW();
+  myCAM.set_fifo_burst();
+  while ( length-- )
+  {
+    temp_last = temp;
+    temp =  SPI.transfer(0x00);
+    //Read JPEG data from FIFO
+    if ( (temp == 0xD9) && (temp_last == 0xFF) ) //If find the end ,break while,
+    {
+      buf[i++] = temp;  //save the last  0XD9
+      //Write the remain bytes in the buffer
+      myCAM.CS_HIGH();
+      file.write(buf, i);
+      //Close the file
+      file.close();
+      Serial.println(F("Image save OK."));
+      is_header = false;
+      i = 0;
+    }
+    if (is_header == true)
+    {
+      //Write image data to buffer if not full
+      if (i < 256)
+        buf[i++] = temp;
+      else
+      {
+        //Write 256 bytes image data to file
+        myCAM.CS_HIGH();
+        file.write(buf, 256);
+        i = 0;
+        buf[i++] = temp;
+        myCAM.CS_LOW();
+        myCAM.set_fifo_burst();
+      }
+    }
+    else if ((temp == 0xD8) & (temp_last == 0xFF))
+    {
+      is_header = true;
+      buf[i++] = temp_last;
+      buf[i++] = temp;
+    }
+  }
+}
+void setup() {
+  uint8_t vid, pid;
+  uint8_t temp;
+  static int i = 0;
+  //set the CS as an output:
+  pinMode(CS, OUTPUT);
+  pinMode(CAM_POWER_ON , OUTPUT);
+  digitalWrite(CAM_POWER_ON, HIGH);
+  Wire.begin();
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(500);
+  }
+  Serial.println("WiFi connected");
+  Serial.println("");
+  Serial.println(WiFi.localIP());
+  Serial.println(F("ArduCAM Start!"));
+  //initialize SPI:
+  SPI.begin();
+  //Reset the CPLD
+  myCAM.write_reg(0x07, 0x80);
+  delay(100);
+  myCAM.write_reg(0x07, 0x00);
+  delay(100);
+  while (1) {
+    //Check if the ArduCAM SPI bus is OK
+    myCAM.write_reg(ARDUCHIP_TEST1, 0x55);
+    temp = myCAM.read_reg(ARDUCHIP_TEST1);
+    if (temp != 0x55) {
+      Serial.println(F("SPI interface Error!"));
+      delay(2);
+      continue;
+    }
+    else
+      break;
+  }
+
+  //Add support for SPIFFS
+  if (!SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED)) {
+    Serial.println("SPIFFS Mount Failed");
+    return;
+  }
+  Serial.println("SPIFFS Mount Successful");
+#if defined (OV2640_MINI_2MP)||defined (OV2640_MINI_2MP_PLUS) || defined (OV2640_CAM)
+  //Check if the camera module type is OV2640
+  myCAM.wrSensorReg8_8(0xff, 0x01);
+  myCAM.rdSensorReg8_8(OV2640_CHIPID_HIGH, &vid);
+  myCAM.rdSensorReg8_8(OV2640_CHIPID_LOW, &pid);
+  if ((vid != 0x26 ) && (( pid != 0x41 ) || ( pid != 0x42 )))
+    Serial.println(F("Can't find OV2640 module!"));
+  else
+    Serial.println(F("OV2640 detected."));
+#elif defined (OV5640_MINI_5MP_PLUS) || defined (OV5640_CAM)
+  //Check if the camera module type is OV5640
+  myCAM.wrSensorReg16_8(0xff, 0x01);
+  myCAM.rdSensorReg16_8(OV5640_CHIPID_HIGH, &vid);
+  myCAM.rdSensorReg16_8(OV5640_CHIPID_LOW, &pid);
+  if ((vid != 0x56) || (pid != 0x40))
+    Serial.println(F("Can't find OV5640 module!"));
+  else
+    Serial.println(F("OV5640 detected."));
+#elif defined (OV5642_MINI_5MP_PLUS) || defined (OV5642_MINI_5MP) || defined (OV5642_MINI_5MP_BIT_ROTATION_FIXED) ||(defined (OV5642_CAM))
+  //Check if the camera module type is OV5642
+  myCAM.wrSensorReg16_8(0xff, 0x01);
+  myCAM.rdSensorReg16_8(OV5642_CHIPID_HIGH, &vid);
+  myCAM.rdSensorReg16_8(OV5642_CHIPID_LOW, &pid);
+  if ((vid != 0x56) || (pid != 0x42)) {
+    Serial.println(F("Can't find OV5642 module!"));
+  }
+  else
+    Serial.println(F("OV5642 detected."));
+#endif
+  //Change to JPEG capture mode and initialize the OV2640 module
+  myCAM.set_format(JPEG);
+  myCAM.InitCAM();
+#if defined (OV2640_MINI_2MP)||defined (OV2640_MINI_2MP_PLUS) || defined (OV2640_CAM)
+  myCAM.OV2640_set_JPEG_size(OV2640_320x240);
+#elif defined (OV5640_MINI_5MP_PLUS) || defined (OV5640_CAM)
+  myCAM.write_reg(ARDUCHIP_TIM, VSYNC_LEVEL_MASK);   //VSYNC is active HIGH
+  myCAM.OV5640_set_JPEG_size(OV5640_320x240);
+#elif defined (OV5642_MINI_5MP_PLUS) || defined (OV5642_MINI_5MP) || defined (OV5642_MINI_5MP_BIT_ROTATION_FIXED) ||(defined (OV5642_CAM))
+  myCAM.write_reg(ARDUCHIP_TIM, VSYNC_LEVEL_MASK);   //VSYNC is active HIGH
+  myCAM.OV5642_set_JPEG_size(OV5642_320x240);
+#endif
+  delay(1000);
+}
+void loop() {
+  sprintf((char*)pname, "/%05d.jpg", k);
+  capture2SD(SPIFFS, pname);
+
+
+ listDir(SPIFFS, "/", 0);
+ readImage(SPIFFS, pname);
+ SendCapturedImage2GoogleDrive(SPIFFS, pname);
+ deleteFile(SPIFFS, pname);
+   k++;
+  delay(5000);
+
+}
+
+void listDir(fs::FS &fs, const char * dirname, uint8_t levels) {
+  Serial.printf("Listing directory: %s\r\n", dirname);
+
+  File root = fs.open(dirname);
+  if (!root) {
+    Serial.println("- failed to open directory");
+    return;
+  }
+  if (!root.isDirectory()) {
+    Serial.println(" - not a directory");
+    return;
+  }
+
+  File file = root.openNextFile();
+  while (file) {
+    if (file.isDirectory()) {
+      Serial.print("  DIR : ");
+      Serial.println(file.name());
+      if (levels) {
+        listDir(fs, file.name(), levels - 1);
+      }
+    } else {
+      Serial.print("  FILE: ");
+      Serial.print(file.name());
+      Serial.print("\tSIZE: ");
+      Serial.println(file.size());
+    }
+    file = root.openNextFile();
+  }
+}
+
+void readImage(fs::FS &fs, const char * path) {
+  Serial.printf("Reading Image: %s\r\n", path);
+  File file = fs.open(path);
+  if (!file || file.isDirectory()) {
+    Serial.println("- failed to open file for reading");
+    return;
+  }
+
+  Serial.println("- read from file:");
+  while (file.available()) {
+    Serial.println("file read successful");
+    break;
+  }
+}
+
+
+
+void deleteFile(fs::FS &fs, const char * path){
+    Serial.printf("Deleting file: %s\r\n", path);
+    if(fs.remove(path)){
+        Serial.println("- file deleted");
+    } else {
+        Serial.println("- delete failed");
+    }
+}
+
+String SendCapturedImage2GoogleDrive(fs::FS &fs, const char * path) {
+
+  Serial.printf("Reading Image: %s\r\n", path);
+  File file = fs.open(path);
+  uint8_t *fileinput;
+  unsigned int fileSize = file.size();
+  fileinput = (uint8_t*)malloc(fileSize + 1);
+  file.read(fileinput, fileSize);
+  fileinput[fileSize] = '\0';
+  file.close();
+
+  char *input = (char *)fileinput;
+  String imageFile = "data:image/jpeg;base64,";
+  char output[base64_enc_len(3)];
+  for (int i = 0; i<fileSize;i++)
+  {
+    base64_encode(output, (input++), 3);
+    if (i%3==0) imageFile += urlencode(String(output));
+    Serial.print(i);
+  }
+
+  String Data = myFoldername+myFilename+myImage;
+  const char* myDomain = "script.google.com";
+  String getAll="", getBody = "";
+    
+  Serial.println("Connect to " + String(myDomain));
+  WiFiClientSecure client_tcp;
+  //client_tcp.setInsecure();   //run version 1.0.5 or above
+  
+  if (client_tcp.connect(myDomain, 443)) {
+    Serial.println("Connection successful");
+     
+    client_tcp.println("POST " + myScript + " HTTP/1.1");
+    client_tcp.println("Host: " + String(myDomain));
+    client_tcp.println("Content-Length: " + String(Data.length()+imageFile.length()));
+    client_tcp.println("Content-Type: application/x-www-form-urlencoded");
+    client_tcp.println("Connection: keep-alive");
+    client_tcp.println();
+    
+    client_tcp.print(Data);
+    int Index;
+    for (Index = 0; Index < imageFile.length(); Index = Index+1000) {
+      client_tcp.print(imageFile.substring(Index, Index+1000));
+    }
+        
+    int waitTime = 10000;   // timeout 10 seconds
+    long startTime = millis();
+    boolean state = false;
+    
+    while ((startTime + waitTime) > millis()) {
+      Serial.print(".");
+      delay(100);      
+      while (client_tcp.available()) {
+          char c = client_tcp.read();
+          if (state==true) getBody += String(c);        
+          if (c == '\n') {
+            if (getAll.length()==0) state=true; 
+            getAll = "";
+          } else if (c != '\r')
+            getAll += String(c);
+          startTime = millis();
+       }
+       if (getBody.length()>0) break;
+    }
+    client_tcp.stop();
+    Serial.println(getBody);
+  }
+  else {
+    getBody="Connected to " + String(myDomain) + " failed.";
+    Serial.println("Connected to " + String(myDomain) + " failed.");
+  }
+  return getBody;
+}
+
+String urlencode(String str)
+{
+    String encodedString="";
+    char c;
+    char code0;
+    char code1;
+    char code2;
+    for (int i =0; i < str.length(); i++){
+      c=str.charAt(i);
+      if (c == ' '){
+        encodedString+= '+';
+      } else if (isalnum(c)){
+        encodedString+=c;
+      } else{
+        code1=(c & 0xf)+'0';
+        if ((c & 0xf) >9){
+            code1=(c & 0xf) - 10 + 'A';
+        }
+        c=(c>>4)&0xf;
+        code0=c+'0';
+        if (c > 9){
+            code0=c - 10 + 'A';
+        }
+        code2='\0';
+        encodedString+='%';
+        encodedString+=code0;
+        encodedString+=code1;
+        //encodedString+=code2;
+      }
+      yield();
+    }
+    return encodedString;
+}
